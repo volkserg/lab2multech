ce6b9cd5d297407d3bf73d9530bdf404a59a2a72
ssvolkov <ssvolkov@ssvolkov.isa.ru>
2019-03-06 13:13:23 +0300

    core class

--- main.py
+++ main.py
@@ -16,12 +16,21 @@ class Image:
 
     def get_pixels(self, rows):
         if self.type=='P2':
+            q = 0
             self.pixels = []
+            pixels = []
             for i in rows:
-                current_row = i.split(' ')
+                current_row = i.strip().split(' ')
                 if current_row[len(current_row)-1] == '':
                     current_row.pop()
-                self.pixels.append([int(j) for j in current_row])
+                for j in current_row:
+                    pixels.append(int(j))
+            for i in range(self.height):
+                temp_row = []
+                for j in range(self.width):
+                    temp_row.append(pixels[q])
+                    q += 1
+                self.pixels.append(temp_row)
 
         if self.type == 'P3':
             self.pixels = {}
@@ -64,6 +73,49 @@ class Image:
                         f.write(str(self.pixels['b'][i][j]) + " ")
                     f.write("\n")
 
+    def _conv_pixel(self, m1, m2):
+        res = 0
+        # print(m1, m2)
+        for i in range(len(m1)):
+            for j in range(len(m1[0])):
+                res += m1[i][j]*m2[i][j]
+        return res
+
+    # def _exp(self, coresize):
+    #     size = int(coresize/2)
+    #     new_img = []
+    #     for i in range(0, self.height + 2 * size):
+    #         row = []
+    #         for j in range(0, self.width + 2 * size):
+    #             if ((j < size) or (j >= self.width + size)) or ((i < size) or (i>=self.height+size)):
+    #                 row.append(0)
+    #             else:
+    #                 row.append(self.pixels[i-size][j-size])
+    #         new_img.append(row)
+    #     return new_img
+
+    def change(self, pixels, core):
+        new_pixels = []
+        img = pixels #self._exp(core.size)
+        for i in range(len(img)-core.size+1):
+            new_pixel_row = []
+            for j in range(len(img[0])-core.size+1):
+                matrix = []
+                rows = img[i:i+core.size]
+                for row in rows:
+                    matrix.append(row[j:j+core.size])
+                new_pixel_row.append(self._conv_pixel(matrix, core.matrix))
+            new_pixels.append(new_pixel_row)
+        return new_pixels
+
+    def make_filter(self, core):
+        if self.type == 'P2':
+            pixels = self.change(self.pixels, core)
+            for pix in pixels:
+                print(pix)
+
+
+
 
 class Core:
 
@@ -76,8 +128,7 @@ class Core:
         with open(filename, 'r') as f:
             file = f.read()
             rows = file.split('\n')
-            self.width = len(rows[0].split(' '))
-            self. height = len(rows)
+            self.size = len(rows)
             for row in rows:
                 current_row = row.split(' ')
                 self.matrix.append([int(i) for i in current_row])
